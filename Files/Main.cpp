41c223db49739da5937c49122d6f7377c0a90c00
login <nikolovi155@gmail.com>
2025-02-08 18:11:25 +0200

    Task3

--- Files/Main.cpp
+++ Files/Main.cpp
@@ -58,13 +58,67 @@ void copyFileContentReverse(const std::string& sourceFile, const std::string& de
     outFile.close();
 }
 
+void processFile(const std::string& sourceFile, const std::string& destFile) {
+    std::ifstream inFile(sourceFile);
+    if (!inFile.is_open()) {
+        std::cout << "Error: Cannot open source file " << sourceFile << std::endl;
+        return;
+    }
+
+    std::ofstream outFile(destFile);
+    if (!outFile.is_open()) {
+        std::cout << "Error: Cannot open destination file " << destFile << std::endl;
+        inFile.close();
+        return;
+    }
+
+    std::string line;
+    int lastLineWithoutSpace = -1;
+    int currentLine = 0;
+    std::string allContent;
+
+    while (std::getline(inFile, line)) {
+        if (line.find(' ') == std::string::npos) {
+            lastLineWithoutSpace = currentLine;
+        }
+        allContent += line + '\n';
+        currentLine++;
+    }
+
+    inFile.clear();
+    inFile.seekg(0);
+
+    currentLine = 0;
+    std::string dashLine(12, '-');
+
+    while (std::getline(inFile, line)) {
+        outFile << line << std::endl;
+
+        if (currentLine == lastLineWithoutSpace) {
+            outFile << dashLine << std::endl;
+        }
+        currentLine++;
+    }
+
+    if (lastLineWithoutSpace == -1) {
+        outFile << dashLine << std::endl;
+    }
+
+    std::cout << "File has been processed successfully!" << std::endl;
+
+    inFile.close();
+    outFile.close();
+}
+
 int main() {
     std::string sourceFileName = "source.txt";
     std::string destFileName = "destination.txt";
 
     // copyFileContent(sourceFileName, destFileName);
 
-    copyFileContentReverse(sourceFileName, destFileName);
+    // copyFileContentReverse(sourceFileName, destFileName);
+
+    processFile(sourceFileName, destFileName);
 
     return 0;
 }
